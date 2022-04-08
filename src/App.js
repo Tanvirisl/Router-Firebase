1ab23e25e9b23f2e093dc6b01d3e7d7ece6edc93
tanvir <himonislamfre@gmail.com>
2022-04-08 22:30:10 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Header from './Component/Header/Header';
+import Home from './Component/Home/Home';
+import Login from './Component/Login/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+        <Routes>
+          <Route path='/' element={<Home></Home>}></Route>
+          <Route path='/login' element={<Login></Login>}></Route>
+        </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.innit.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCauov_PdQDzwJdd4Ta8jiWhuFloPF0n_c",
+  authDomain: "router-firebase-c56d3.firebaseapp.com",
+  projectId: "router-firebase-c56d3",
+  storageBucket: "router-firebase-c56d3.appspot.com",
+  messagingSenderId: "1003549132549",
+  appId: "1:1003549132549:web:57c0d580a3f16bcca1d96c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
